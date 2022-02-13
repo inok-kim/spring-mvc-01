a51f3c4f13c8e3a3c4143137eb6cf83fec889370
iokim <iokim1220@gmail.com>
2022-02-13 22:43:14 +0900

    # 뷰 리졸버

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +13,11 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	// springboot가 설정해서 다 해주기 때문에 아래 코드 직접 작성하지 않아도됨
+//	@Bean
+//	InternalResourceViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB-INF/views/",".jsp");
+//	}
 
 }
