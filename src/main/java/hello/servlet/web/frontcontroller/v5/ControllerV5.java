8ecf1a79847bb09845fa7eb5c938ca6299af45f3
inok-kim <iokim1220@gmail.com>
2022-02-13 18:24:58 +0900

    # 유연한 컨트롤러2 - V5

--- src/main/java/hello/servlet/web/frontcontroller/v5/ControllerV5.java
+++
@@ -1,9 +0,0 @@
-package hello.servlet.web.frontcontroller.v5;
-
-import hello.servlet.web.frontcontroller.ModelView;
-
-import java.util.Map;
-
-public interface ControllerV5 {
-    ModelView process(Map<String, String> paramMap);
-}
